74dbb0e4b55a15326e10d7e4fa1a42bf2fe10155
jinjunliang <jjl@nbdeli.com>
2020-07-24 10:46:39 +0800

    add schedule demo

---
+++ src/main/java/io/frank/learn/basic/SchedulerDemo.java
@@ -0,0 +1,38 @@
+package io.frank.learn.basic;
+
+import org.omg.PortableServer.THREAD_POLICY_ID;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+/**
+ * @author jinjunliang
+ */
+public class SchedulerDemo {
+    public static void main(String[] args) throws Exception {
+        Scheduler s = Schedulers.newParallel("parallel-scheduler", 4);
+
+        final Flux<String> flux = Flux
+                .range(1, 2)
+                .map(i -> {
+                    System.out.println("first map happens:" + Thread.currentThread().getName());
+                    return 10 + i;
+                })
+                .publishOn(s)
+                .map(i -> {
+                    System.out.println("second map happens" + Thread.currentThread().getName());
+                    return "value " + i;
+                });
+
+        new Thread(() -> {
+            Disposable d = flux.subscribe(
+                    i -> System.out.println("subscription happens" + Thread.currentThread().getName()),
+                    e -> e.printStackTrace(),
+                    () -> System.out.println("complete")
+            );
+        }).run();
+
+        Thread.sleep(1000);
+    }
+}
